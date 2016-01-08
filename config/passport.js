9477d1555ac0c8040b9348fa82c03b706b3b55ab
Dan McQuillan <djmcquillan76@gmail.com>
2016-01-07 21:49:15 -0800

    removed facebook login option and facebook references

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,5 @@
 var passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy,
-  FacebookStrategy = require('passport-facebook').Strategy,
   MeetupStrategy   = require('passport-meetup').Strategy,
   configAuth = require('./auth.js');
 
@@ -54,34 +53,6 @@ passport.use('local-login', new LocalStrategy({
       if(!user) return done(null, false, req.flash('loginMessage', 'No user found.'))
       if(!user.validPassword(password)) return done(null, false, req.flash('loginMessage', 'Invalid email/password'))
       return done(null, user)
-    })
-  }))
-
-// Passport facebook strategy
-passport.use(new FacebookStrategy({
-    clientID: configAuth.facebookAuth.clientID,
-    clientSecret: configAuth.facebookAuth.clientSecret,
-    callbackURL: configAuth.facebookAuth.callbackURL,
-    profileFields: configAuth.facebookAuth.profileFields
-}, 
-  function(token, refreshToken, profile, done){
-    User.findOne({'facebook.id': profile.id}, function(err, user){
-      if(err) return done(err)
-      if(user) {
-        return done(null, user)
-      }
-      else {
-        var newUser = new User()
-        newUser.facebook.id = profile.id
-        newUser.facebook.token = token
-        newUser.facebook.name = profile.displayName
-        newUser.facebook.email = profile.emails[0].value
-
-        newUser.save(function(err){
-          if(err) throw err
-          return done(null, newUser)
-        })
-      }
     })
   }))
 
